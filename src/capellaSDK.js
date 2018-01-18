dacb9da6086d8c30bda01908c5ffc4bec6802c39
vovaaksenov99 <vovaaksenov99@mail.ru>
2018-01-19 00:46:51 +0300

    refactoring

--- src/capellaSDK.js
+++ src/capellaSDK.js
@@ -10,22 +10,19 @@ let fs = require('fs');
 
 module.exports = function (capellaSDK) {
 
-  /**
-   * Use Capella API for the image uploading
-   *
-   * {@link https://github.com/codex-team/capella}
-   *
-   * @param {String} imagePath local path to image
-   * @param {Function} callback action after upload picture
-   */
-  capellaSDK.uploadImageToCapella = function (imagePath, callback) {
+  let getAnswer = function (json) {
     try {
-      let req = request.post('https://capella.pics/upload', callback);
-      let form = req.form();
-      form.append('file', fs.createReadStream(imagePath));
+      json = JSON.parse(json);
+
+      if (json.success == false) {
+        return json.message;
+      }
+      else {
+        return json;
+      }
     }
     catch (exception) {
-      return exception;
+      throw exception;
     }
   };
 
@@ -34,17 +31,47 @@ module.exports = function (capellaSDK) {
    *
    * {@link https://github.com/codex-team/capella}
    *
-   * @param {String} URL url path to image
-   * @param {Function} callback action after upload picture
+   * @param {String} - imagePath local path to image
+   * @param {Function} - callback action after upload picture
    */
-  capellaSDK.uploadImageToCapellaURL = function (URL, callback) {
+  capellaSDK.uploadFile = function (imagePath, callback) {
     try {
-      let req = request.post('https://capella.pics/upload', callback);
+      let req = request.post('https://capella.pics/upload', function (error, resp, body) {
+
+        if (error != null) {
+          throw error;
+        }
+        callback(getAnswer(body));
+      });
+      let form = req.form();
+      form.append('file', fs.createReadStream(imagePath));
+    }
+    catch (exception) {
+      throw  exception;
+    }
+  };
+
+  /**
+   * Use Capella API for the image uploading
+   *
+   * {@link https://github.com/codex-team/capella}
+   *
+   * @param {String} - URL url path to image
+   * @param {Function} - callback action after upload picture
+   */
+  capellaSDK.uploadFileByURL = function (URL, callback) {
+    try {
+      let req = request.post('https://capella.pics/upload', function (error, resp, body) {
+        if (error != null) {
+          throw error;
+        }
+        callback(getAnswer(body));
+      });
       let form = req.form();
       form.append('link', URL);
     }
     catch (exception) {
-      return exception;
+      throw exception;
     }
   };
 
