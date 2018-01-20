e432da88307b336cb9f0b93c6106aade33c03048
vovaaksenov99 <vovaaksenov99@mail.ru>
2018-01-20 15:29:09 +0300

    Refactoring

--- src/capella.js
+++ src/capella.js
@@ -17,11 +17,11 @@ class Capella {
     }
 
     /**
-   * Get and parse response from server
-   *
-   * @param {String} json - JSON string with the server response
-   * @return {object|string} - parsed response or error message
-   */
+     * Get and parse response from server
+     *
+     * @param {String} json - JSON string with the server response
+     * @return {object|string} - parsed response or error message
+     */
     parseResponse(json) {
         json = JSON.parse(json);
 
@@ -33,32 +33,32 @@ class Capella {
     };
 
     /**
-   * Create request for post image to Capella
-   *
-   * Callback argument description
-   * On error
-   * {
-   *    "success": false,
-   *    "message": <message>
-   * }
-   * On success
-   * {
-   *    "success": true,
-   *    "message": <message>,
-   *    "id": <id>,
-   *    "url": <url>
-   * }
-   *
-   * @param {Function} callback - callback action after upload picture.
-   * Callback argument has format
-   * @return {Function} - request query
-   */
+     * Create request for post image to Capella
+     *
+     * Callback argument description
+     * On error
+     * {
+     *    "success": false,
+     *    "message": <message>
+     * }
+     * On success
+     * {
+     *    "success": true,
+     *    "message": <message>,
+     *    "id": <id>,
+     *    "url": <url>
+     * }
+     *
+     * @param {Function} callback - callback action after upload picture.
+     * Callback argument has format
+     * @return {Function} - request query
+     */
     createRequest(callback) {
         let req = request.post(this.endpoint, (error, resp, body) => {
             if (error != null) {
                 error = {
-                    'success':false,
-                    'message':error.code + ' ' + error.message
+                    'success': false,
+                    'message': error.code + ' ' + error.message
                 };
                 callback(error);
                 return;
@@ -69,8 +69,8 @@ class Capella {
                 response = this.parseResponse(body);
             } catch (exception) {
                 response = {
-                    'success':false,
-                    'message':'Invalid JSON format'
+                    'success': false,
+                    'message': 'Invalid JSON format'
                 };
             }
             callback(response);
@@ -80,13 +80,13 @@ class Capella {
     }
 
     /**
-   * Use Capella API for the image uploading
-   *
-   * {@link https://github.com/codex-team/capella}
-   *
-   * @param {String} imagePath - local path to image
-   * @param {Function} callback - action after upload picture
-   */
+     * Use Capella API for the image uploading
+     *
+     * {@link https://github.com/codex-team/capella}
+     *
+     * @param {String} imagePath - local path to image
+     * @param {Function} callback - action after upload picture
+     */
     uploadFile(imagePath, callback) {
         let req = this.createRequest(callback);
         let form = req.form();
@@ -95,13 +95,13 @@ class Capella {
     };
 
     /**
-   * Use Capella API for the image uploading
-   *
-   * {@link https://github.com/codex-team/capella}
-   *
-   * @param {String} URL - URL url path to image
-   * @param {Function} callback - action after upload picture
-   */
+     * Use Capella API for the image uploading
+     *
+     * {@link https://github.com/codex-team/capella}
+     *
+     * @param {String} URL - URL url path to image
+     * @param {Function} callback - action after upload picture
+     */
     uploadFileByURL(URL, callback) {
         let req = this.createRequest(callback);
         let form = req.form();
